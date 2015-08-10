a71512695aea06ed38a3cf0faeed217b1aa04758
Bob van der Linden <bobvanderlinden@gmail.com>
2015-08-10 22:46:57 +0200

    use pyzeroconf instead of avahi (through dbus)
    This adds better compatibility for Windows and also removes the
    need for running Avahi and DBus on the system.

--- zeroconfbrowser.py
+++ zeroconfbrowser.py
@@ -1,16 +1,8 @@
 import sys
 import os
-import dbus
-from dbus.mainloop.glib import DBusGMainLoop
-import avahi
-import gobject
-import threading
 from collections import namedtuple
 from functools import partial
-
-# TODO: Do we need this here?
-gobject.threads_init()
-dbus.mainloop.glib.threads_init()
+from zeroconf import Zeroconf, ServiceBrowser, ServiceStateChange
 
 # Data types
 AvahiServiceDescription = namedtuple("AvahiService", ["interface", "protocol", "name", "service", "domain", "flags"])
@@ -21,129 +13,68 @@ MachineService = namedtuple("MachineService", ["name", "dsn"])
 class ZeroconfBrowser():
     """Searches the network for machine services"""
     def __init__(self, on_machine_discovered=None, on_service_discovered=None, on_initial_discovery_finished=None, on_failure=None):
-        self.service_browsers = set()
+        self.service_browsers = dict()
         self.resolving_services = set()
-        self.discovery_finshed = False
         self.machines = dict()
         self.on_machine_discovered = on_machine_discovered
         self.on_service_discovered = on_service_discovered
         self.on_initial_discovery_finished = on_initial_discovery_finished
         self.on_failure = on_failure
-        self.lock = threading.Lock()
 
-        # TODO: Make sure this isn't this a global loop.
-        loop = DBusGMainLoop(set_as_default=True)
-        self._bus = dbus.SystemBus(mainloop=loop)
-        self.server = dbus.Interface(
-                self._bus.get_object(avahi.DBUS_NAME, avahi.DBUS_PATH_SERVER),
-                avahi.DBUS_INTERFACE_SERVER
-                )
-
-        thread = threading.Thread(target=gobject.MainLoop().run)
-        thread.daemon = True
-        thread.start()
+        self.zeroconf = Zeroconf()
 
         # machinekit services are here:
-        self.browse("_machinekit._tcp")
+        self.browse("_machinekit._tcp.local.")
 
         # the webtalk server announces _http or _https as configured
-        self.browse("_http._tcp")
-        self.browse("_https._tcp")
+        self.browse("_http._tcp.local.")
+        self.browse("_https._tcp.local.")
 
-    def browse(self, service):
-        if service in self.service_browsers:
-            return
-        self.service_browsers.add(service)
+    def __enter__(self):
+        pass
 
-        with self.lock:
-            browser = dbus.Interface(self._bus.get_object(avahi.DBUS_NAME,
-                    self.server.ServiceBrowserNew(avahi.IF_UNSPEC,
-                            avahi.PROTO_UNSPEC, service, "local", dbus.UInt32(0))),
-                    avahi.DBUS_INTERFACE_SERVICE_BROWSER)
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.zeroconf.close()
 
-            browser.connect_to_signal("ItemNew", self.item_new)
-            browser.connect_to_signal("AllForNow", self.all_for_now)
-            browser.connect_to_signal("Failure", self.failure)
-
-    def pair_to_dict(self, l):
-        ''' helper to parse TXT record into dict '''
-        res = dict()
-        for el in l:
-            if "=" not in el:
-                res[el]=''
-            else:
-                tmp = el.split('=',1)
-                if len(tmp[0]) > 0:
-                    res[tmp[0]] = tmp[1]
-        return res
-
-    def item_new(self, interface, protocol, name, service, domain, flags):
-        with self.lock:
-            avahiServiceDescription = AvahiServiceDescription(interface, protocol, name, service, domain, flags)
-            self.resolving_services.add(avahiServiceDescription)
-            self.server.ResolveService(
-                    interface,
-                    protocol,
-                    name,
-                    service,
-                    domain,
-                    avahi.PROTO_UNSPEC,
-                    dbus.UInt32(0),
-                    reply_handler=self.resolved,
-                    error_handler=partial(self.resolve_error, avahiServiceDescription)
-                    )
-
-    def all_for_now(self):
-        with self.lock:
-            if self.discovery_finshed:
-                return
-            self.discovery_finshed = True
-
-            # Check whether we are done resolving/discovering all machines.
-            self.checkAllForNow()
-
-    def checkAllForNow(self):
-        if self.discovery_finshed and len(self.resolving_services) == 0:
-            if not self.on_initial_discovery_finished is None:
-                self.on_initial_discovery_finished(self.machines)
-
-    def failure(self, exception):
-        if not self.on_failure is None:
-            self.on_failure(exception)
-
-    def resolved(self, interface, protocol, name, service, domain, host,
-            aprotocol, address, port, txt, flags):
-        avahiServiceDescription = AvahiServiceDescription(interface, protocol, name, service, domain, flags)
-        if not avahiServiceDescription in self.resolving_services:
-            # The service was already removed. No need to do anything with the resolved service information.
-            return
-        self.resolving_services.remove(avahiServiceDescription)
-
-        self.handleTxtEntry(txt)
-
-        # Check whether we are done resolving/discovering all machines.
-        self.checkAllForNow()
-
-    def handleTxtEntry(self, txt):
-        # Check whether all required machine attributes are in the txt field.
-        tr = self.pair_to_dict(avahi.txt_array_to_string_array(txt))
-        if not "uuid" in tr:
-            return
-        if not "dsn" in tr:
-            return
-        if not "service" in tr:
+    def browse(self, serviceName):
+        if serviceName in self.service_browsers:
             return
 
-        # We found a valid Machinekit service.
+        browser = ServiceBrowser(self.zeroconf, serviceName, handlers=[self.on_service_state_change])
+        self.service_browsers[serviceName] = browser
 
-        uuid = tr["uuid"]
-        dsn = tr["dsn"]
-        service = tr["service"]
+    def on_service_state_change(self, zeroconf, service_type, name, state_change):
+        if state_change is ServiceStateChange.Added:
+            info = zeroconf.get_service_info(service_type, name)
+            if info:
+                self.on_service_added(zeroconf, service_type, name, info)
 
-        # Check if the service belongs to a new machine.
+        elif state_change is ServiceStateChange.Removed:
+            # TODO: Implement
+            pass
+
+    def on_service_added(self, zeroconf, service_type, name, info):
+        # Validate service info.
+        if not info.properties:
+            return
+        if b"uuid" not in info.properties:
+            return
+        if b"service" not in info.properties:
+            return
+        if b"dsn" not in info.properties:
+            return
+
+        # Retrieve wanted properties from service info.
+        uuid = info.properties[b"uuid"].decode('utf-8')
+        dsn = info.properties[b"dsn"].decode('utf-8')
+        service = info.properties[b"service"].decode('utf-8')
+
+        # See if machine was already discovered.
         if not uuid in self.machines:
             machine = Machine(uuid = uuid, services = dict())
             self.machines[uuid] = machine
+
+            # Notify consumer about discovered machine.
             if not self.on_machine_discovered is None:
                 self.on_machine_discovered(machine)
 
@@ -157,11 +88,3 @@ class ZeroconfBrowser():
         machine.services[machineService.name] = machineService
         if not self.on_service_discovered is None:
             self.on_service_discovered(machine, machineService)
-
-    def resolve_error(self, avahiServiceDescription, *args, **kwargs):
-        if not avahiServiceDescription in self.resolving_services:
-            return
-        self.resolving_services.remove(avahiServiceDescription)
-
-        # Check whether we are done resolving/discovering all machines.
-        self.checkAllForNow()
